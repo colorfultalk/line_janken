1b07cc30389d34564a4c2f36a33bc17f4ab789d4
Yoshiharu-Ikutani <ikutani.yoshiharu.ip8@is.naist.jp>
2017-07-20 16:43:49 +0900

    initial

---
+++ models.py
@@ -0,0 +1,26 @@
+from peewee import *
+from urllib.parse import urlparse
+import psycopg2, os
+
+# for heroku db connection
+url = urlparse(os.environ["DATABASE_URL"])
+db  = PostgresqlDatabase(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+     )
+
+# model definition
+class Player(Model):
+    lineId      = BigIntegerField()
+    displayName = CharField()
+
+    class Meta:
+        database = db
+
+    def __init__(self, lineId, displayName):
+        super(Guest, self).__init__()
+        self.lineId = lineId
+        self.displayName = displayName
