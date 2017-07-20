3ad50e0cad2034a2f30bc16f7f116e79894d2965
Yoshiharu-Ikutani <ikutani.yoshiharu.ip8@is.naist.jp>
2017-07-20 17:25:03 +0900

    save player

--- janken.py
+++ janken.py
@@ -7,6 +7,9 @@ from linebot.models import (
 )
 import os
 
+# user defined modules
+from models import Player
+
 app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
@@ -30,6 +33,16 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+  # get user_id from event
+  lId = event.source.user_id
+  profile = line_bot_api.get_profile(uId)
+  name = profile.display_name
+
+  # save Player
+  player = Player(lineId = lId, displayName = name)
+  player.save()
+
+  # reply
   line_bot_api.reply_message(
     event.reply_token,
     TextSendMessage(text=event.message.text)
